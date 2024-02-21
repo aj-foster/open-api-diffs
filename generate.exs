ea9e5ac8488ac1dbfef944d9372a13fb1df66baa
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-02-21 16:10:11 -0500

    Add initial workflow

---
+++ generate.exs
@@ -0,0 +1,45 @@
+#
+# Install
+#
+
+ref =
+  case System.argv() do
+    [ref] ->
+      ref
+
+    _else ->
+      raise "Invalid invocation: please pass the ref of aj-foster/open-api-generator to install"
+  end
+
+Mix.install([
+  {:oapi_generator, github: "aj-foster/open-api-generator", ref: ref}
+])
+
+#
+# Configuration
+#
+
+require Config
+
+Path.join(__DIR__, "specs/config.exs")
+|> Config.Reader.read!()
+|> Application.put_all_env()
+
+#
+# Generate
+#
+
+spec_files = Path.join(__DIR__, "specs/*.{json,yaml,yml}")
+
+for spec_file <- Path.wildcard(spec_files) do
+  spec_name = Path.basename(spec_file) |> Path.rootname()
+  output_directory = Path.join(__DIR__, "output/#{spec_name}")
+
+  File.mkdir_p!(output_directory)
+  File.cd!(output_directory)
+
+  config = Application.get_all_env(:oapi_generator)
+  profile = if Keyword.has_key?(config, String.to_atom(spec_name)), do: spec_name, else: "default"
+
+  OpenAPI.run(profile, [spec_file])
+end
---
+++ specs/config.exs
@@ -0,0 +1,11 @@
+# Configuration for code generation examples
+#
+# If configuration is necessary to test a specification, please add a new
+# configuration profile with the same name as the specification's filename
+# (use underscores instead of dashes; etc.).
+#
+# Please only add minimal profiles as necessary.
+#
+import Config
+
+config :oapi_generator, default: []
